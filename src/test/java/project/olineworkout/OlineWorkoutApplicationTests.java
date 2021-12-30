e3f5399e6ea1bae7bc61a4f3458ceccf19955b79
HyuckJuneHong <jjj8337721@naver.com>
2021-12-30 16:15:20 +0900

    https://github.com/hyuck-study/OlineWorkout-Server-api.git

---
+++ src/test/java/project/olineworkout/OlineWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package project.olineworkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlineWorkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
